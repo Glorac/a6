d56d02b7f9f34185ce78380de8d2ff51ea7cc1be
Glorac <37155620+Glorac@users.noreply.github.com>
2023-01-27 16:31:20 -0600

    Generalize functions for all data types
    Create methods to generalize input and data processing for JSON, XML, and/or TSV files. Allow user to select.

--- a6-script.js
+++ a6-script.js
@@ -5,24 +5,32 @@
  */
 
 // Load relevant modules.
-const csvConverter = require('a6-csvToJSON')
+const convert = require('a6-csvToX')
 const rl = require('readline').createInterface({ input: process.stdin, output: process.stdout });
 
 /**
  * @function getFilePaths
  * @summary Prompts user for file paths via CLI.
  * @requires readline
- * @returns {string} csvPath, jsonPath
+ * @returns {string} [csvPath, outputPath, dataType]
  */
 const getFilePaths = async function () {
-	let csvPath = await new Promise(cp => { rl.question('\nPlease provide a link or file path to your CSV file:  ', cp)});
-	let jsonPath = await new Promise(jp => { rl.question('\nPlease provide a file path or file name to store the converted JSON data:  ', jp)});
+	let dataType = await new Promise(dt => { rl.question('\nWould you like to convert the file to JSON, XML, or TSV?  ', dt)});
+	dataType = dataType.toUpperCase();
 	
-	return [csvPath, jsonPath];
+	while (dataType != 'JSON' && dataType != 'XML' && dataType != 'TSV') {
+		dataType = await new Promise(dt => { rl.question('\nSorry, that wasn\'t valid input. Please try again.\nWould you like to convert the file to JSON, XML, or TSV?  ', dt)});
+		dataType = dataType.toUpperCase();
+	}
+	
+	let csvPath = await new Promise(cp => { rl.question('\nPlease provide a link or file path to your CSV file:  ', cp)});
+	let outputPath = await new Promise(op => { rl.question('\nPlease provide a file path or file name to store the converted data:  ', op)});
+	
+	return [csvPath, outputPath, dataType];
 };
 
 // Run.
 (async () => {
-	let [csvPath, jsonPath] = await getFilePaths();
-	csvConverter(csvPath, jsonPath);
+	let [csvPath, outputPath, dataType] = await getFilePaths();
+	convert(csvPath, outputPath, dataType);
 })().catch(e => {});
