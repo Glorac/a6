9762324d1a60e7040e8151c0f469fd3a043d427c
Glorac <37155620+Glorac@users.noreply.github.com>
2023-01-27 09:12:51 -0600

    Change event handling
    Change .on('data') to .on('readable') and loop through buffered data using .read()

--- a6-script.js
+++ a6-script.js
@@ -27,51 +27,56 @@ const csvToJSON = async function (csvPath, jsonPath) {
 		let headers, overflow;
 		let lineCount = 0;
 		
-		// Read the CSV file.
-		readStream.on('data', d => {		
-			// Parse Rows
-			let rows = d.toString().split('\n');
+		// Read the CSV file as data enters buffer.
+		readStream.on('readable', () => {		
+			let d;
 			
-			// Grab Headers
-			if (data_start) headers = rows[0].split(',');
-		
-			// Loop through rows, converting data to JSON.
-			for (let i = 0; i < rows.length; i++) {
-				// Shift down a row if just beginning to process.
-				if (data_start && i == 0) { i = 1 }				
+			// Loop while data exists in buffer.
+			while (null !== (d = readStream.read())) {
+				// Parse Rows
+				let rows = d.toString().split('\n');
+				
+				// Grab Headers
+				if (data_start) headers = rows[0].split(',');
 			
-				// Check for overflow data from previous chunk and reset overflow.
-				if (overflow) {
-					rows[i] = overflow + rows[i];
-					overflow = false;
-				}
-			
-				// Grab comma-separated values and validate against headers.
-				let values = rows[i].split(',');
-				if (values.length == headers.length) {
-					// Initialize object to hold formatted data.
-					let json = {};
-					
-					// Loop through columns.
-					for (let j = 0; j < headers.length; j++) {
-						json[headers[j]] = values[j];
-						if (!values[j] || values[j] == '') console.log(rows[i]);
+				// Loop through rows, converting data to JSON.
+				for (let i = 0; i < rows.length; i++) {
+					// Shift down a row if just beginning to process.
+					if (data_start && i == 0) { i = 1 }				
+				
+					// Check for overflow data from previous chunk and reset overflow.
+					if (overflow) {
+						rows[i] = overflow + rows[i];
+						overflow = false;
 					}
 				
-					// Write to JSON file.
-					writeStream.write(JSON.stringify(json), (err) => {
-						if (err) console.log(err);
-					});
+					// Grab comma-separated values and validate against headers.
+					let values = rows[i].split(',');
+					if (values.length == headers.length) {
+						// Initialize object to hold formatted data.
+						let json = {};
+						
+						// Loop through columns.
+						for (let j = 0; j < headers.length; j++) {
+							json[headers[j]] = values[j];
+							if (!values[j] || values[j] == '') console.log(rows[i]);
+						}
 					
-					lineCount++;
-				} else {
-					// Store overflow data for the next chunk.
-					overflow = rows[i];
+						// Write to JSON file.
+						writeStream.write(JSON.stringify(json), (err) => {
+							if (err) console.log(err);
+						});
+						
+						lineCount++;
+					} else {
+						// Store overflow data for the next chunk.
+						overflow = rows[i];
+					}
 				}
+			
+				// End Header checking
+				if (data_start) data_start = false;
 			}
-		
-			// End Header checking
-			if (data_start) data_start = false;
 		});
 
 		// End gracefully.
